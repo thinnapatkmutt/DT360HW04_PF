8a8d7ceaa306ed96862ea7b0215c3b2fc0392540
Thinnapat <thinnapat.phopit@mail.kmutt.ac.th>
2023-05-16 16:25:50 +0700

    first push

---
+++ Assets/Pathfinding/Scripts/PathNode.cs
@@ -0,0 +1,57 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode {
+
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public bool isWalkable;
+    public bool isSand;
+    public PathNode cameFromNode;
+
+    public PathNode(Grid<PathNode> grid, int x, int y) {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true;
+        isSand = false;
+    }
+
+    public void CalculateFCost() {
+        fCost = gCost + hCost;
+    }
+
+    public void SetIsWalkable(bool isWalkable) {
+        this.isWalkable = isWalkable;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+    public void SetIsSand(bool isSand)
+    {
+        this.isSand = isSand;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public override string ToString() {
+        return x + "," + y;
+    }
+
+}
